54f5df5d0dbce47d9bb0c4ed6a02dde9d5782c15
Amanda Portela <mandaptela@gmail.com>
2024-01-16 17:50:40 -0300

    adição de dispach para o logout do usuario

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,11 +1,16 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
 const estadoInicialUser = {
   email: '',
+  logged: false
 };
 
 function user(state = estadoInicialUser, action) {
   if (action.type === 'login') {
-    return { email: action.value };
+    return { email: action.value, logged: true };
+  }
+
+  if(action.type === "logout") {
+    return { email: "", logged: false };
   }
   return state;
 }
