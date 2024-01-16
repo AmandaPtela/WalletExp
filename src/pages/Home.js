447795c45ca72876fcdee2d30f1e093d7108c3a9
Amanda Portela <mandaptela@gmail.com>
2024-01-16 17:47:53 -0300

    adição de botão para o usuário deslogar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import Header from '../components/Header';
 import Wallet from './Wallet';
 import '../CSS/Home.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import notification from '../components/notification';
 
 class Home extends React.Component {
   render() {
+    const { dispatch, logged } = this.props;
     return (
       <div id="home">
         <Header />
@@ -15,9 +19,31 @@ class Home extends React.Component {
             <Route path="/" exact element=<Login /> />
             <Route path="/carteira" element=<Wallet /> />
           </Routes>
+
+          {logged ?
+            <div id="btn-logoff">
+              <Link to="/">
+                <button onClick={() => {
+                  notification('Sessão encerrada');
+                  dispatch({ type: 'logout' });
+                }}>Logout</button>
+              </Link>
+            </div>
+            : null
+          }
         </div>
-      </div>
+      </div >
     );
   }
 }
-export default Home;
+
+const mapStateToProps = (state) => ({
+  user: state.user.email,
+  logged: state.user.logged,
+});
+
+Home.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps)(Home);
