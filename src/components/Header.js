33ab34810ad9fd31fb078448f332e43a11d71c99
Amanda Portela <mandaptela@gmail.com>
2024-01-16 17:45:43 -0300

    ajuste na renderização condicional do header e adição de localStorage para guardar o usuario

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,27 +7,30 @@ import { soma } from '../testes/Functions';
 // Feito com consulta ao https://serfrontend.com/blog/redux-com-react-para-iniciantes/index.html
 class Header extends React.Component {
   render() {
-    const { user, despesas, dispatch } = this.props;
+    const { logged, despesas, dispatch } = this.props;
     const somaTotal = soma(despesas, dispatch);
     dispatch({ type: 'somaTotal', value: Number(somaTotal) });
+
+    const user = localStorage.getItem('User');
+    
     return (
-      (user.length > 0)
+      (logged && user && user.length > 0)
         ? (
           <div className="header">
             <div id="logo-header">
               <span className="header-logo">WalletExp</span>
             </div>
+            <div id="total-field">
+              <span data-testid="total-field" id="total">
+                {`R$ ${Number(somaTotal).toFixed(2)}`}
+              </span>
+              <span id="header-currency-field"> Gasto total </span>
+            </div>
             <div id="user-field">
               <span id="header-user-field"> Usuário </span>
               <span id="email-field">
-                { user }
+                {user}
               </span>
-            </div>
-            <div id="total-field">
-              <span data-testid="total-field" id="total">
-                { `R$ ${Number(somaTotal).toFixed(2)}` }
-              </span>
-              <span id="header-currency-field"> Gasto total </span>
             </div>
           </div>
         )
@@ -41,12 +44,13 @@ class Header extends React.Component {
 }
 const mapStateToProps = (state) => ({
   user: state.user.email,
+  logged: state.user.logged,
   despesas: state.wallet.expenses,
 });
 
 Header.propTypes = {
   user: PropTypes.string.isRequired,
-  despesas: PropTypes.arrayOf(PropTypes.string).isRequired,
+  despesas: PropTypes.arrayOf(PropTypes.object).isRequired,
   dispatch: PropTypes.func.isRequired,
 };
 
